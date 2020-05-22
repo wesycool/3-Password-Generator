07383ffd8ba3429e9991646a3835696c6238ad73
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-05-22 14:25:28 -0400

    Shortened code and added comments

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function writePassword() {
 
 }
 
+//Select Passord Criteria
 function generatePassword(){
   var criteria = Number(prompt("Which criteria to include in the password?\n  [1] Length\n  [2] Character Type\n  [3] All the Above"));
   var length = [1,3].includes(criteria) ? getLength() : (Math.floor(Math.random()*121)+8);
@@ -17,6 +18,7 @@ function generatePassword(){
   return getPassword(length,charType);
 }
 
+//Get Length Criteria
 function getLength(){
   do{
     var length = Number(prompt("How long is the password (between 8 to 128)?",8));
@@ -27,6 +29,7 @@ function getLength(){
   return length;
 }
 
+//Get Character Type Criteria
 function getCharType(){
   var charArray = {
     "LOWERCASE":false,
@@ -36,7 +39,7 @@ function getCharType(){
   }
 
   do{
-    Object.keys(charArray).forEach((element,index) => charArray[element] = confirm(`${index + 1} - Do you want to have ${element}?`));
+    Object.keys(charArray).forEach((value,index) => charArray[value] = confirm(`${index + 1} - Do you want to have ${value}?`));
     var charCondition = !Object.values(charArray).includes(true);
     if (charCondition) getError(false);
   }while(charCondition)
@@ -44,37 +47,30 @@ function getCharType(){
   return charArray;
 }
 
+//Get Error Message
 function getError(isLength){
   return alert("Invalid entry, please try again!\nPassword must " + (isLength? "be between 8 to 128 characters." : "have at least 1 character type."))
 }
 
+//Run Random Password
 function getPassword(length, charType){
   var passwordArray = [];
   
   do {
-    var charCondition = [false,false,false,false]
+    var defaultType = [false,false,false,false]
     for (var i=0;i<length;i++){
       do{
-        var randomType = Math.floor(Math.random()*4);
-        var charCode;
-    
-        switch (randomType) {
-          case 0: charCode = Math.floor(Math.random()*26) + 97; break; // lower 97-122
-          case 1: charCode = Math.floor(Math.random()*26) + 65; break; // upper 65-90
-          case 2: charCode = Math.floor(Math.random()*10) + 48; break; // numeric 48-57
-          case 3: charCode = Math.floor(Math.random()*15) + 33; break; // special char !"#$%&'()*+,-./ (33-47)    :;<=>?@ (58-64)  [\]^_`  (91-96) {|}~  (123-126)
-        }
-
-        passwordArray[i] = String.fromCharCode(charCode);
-        charCondition[randomType] = charType[randomType];
-    
-      }while (!charType[randomType]);
+        var charCode = String.fromCharCode(Math.floor(Math.random()*93) + 33);
+        var charCondition = [/[a-z]/,/[A-Z]/,/[0-9]/,/[^a-zA-Z0-9]/];
+        var charIndex;
+        charCondition.forEach((value,index) =>  {if (charCode.match(value) != null) charIndex = index;});
+        defaultType[charIndex] = charType[charIndex]
+        passwordArray[i] = charCode;
+      }while (!charType[charIndex])
     }
-
-  } while (charCondition.join() != charType.join());
+  } while (defaultType.join() != charType.join());
   
   return passwordArray.join('');
-
 }
 
 // Add event listener to generate button
